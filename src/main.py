af370d577135e470c63d14be2dbc7388d46525af
dxx <dx1898@foxmail.com>
2026-06-24 23:59:33 +0800

    Using windows compatible event loop

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 def run():
+    import asyncio
+    import sys
+
     import uvicorn
 
     from config.settings import get_settings
+
+    # 必须在导入其他异步库之前执行
+    if sys.platform == "win32":
+        # Windows 平台强制使用 SelectorEventLoop
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     settings = get_settings()
 
